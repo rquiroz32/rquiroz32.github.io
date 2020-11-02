7fa8983626f9a2f1d094c2d76a6cfea06385b829
rquiroz <rquiroz3290@gmail.com>
2020-11-01 21:50:00 -0500

    adding on click to open a mialto link to me

---
+++ script.js
@@ -0,0 +1,10 @@
+var fullName = $("#fullName").val().trim();
+var emailAddress = $("#emailAddress").val().trim();
+var emailBody = $("#emailBody").val().trim();
+
+$("button").on("click", function(){
+    
+    window.location.href = "mailto:rquiroz@gmail.com";
+
+
+})// closes on click
